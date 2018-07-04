c2b4b1a37a876756b0a5e8b031454fdfbf466d76
Alex Yang <aleozlx@gmail.com>
2018-07-03 22:49:47 -0500

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ struct Matrix {
 }
 
 impl Matrix {
-    // #[no_mangle]
-    // #[inline(never)]
     #[target_feature(enable = "avx2")]
     #[target_feature(enable = "fma")]
     unsafe fn s_product_sum(&mut self, a: &Matrix, b: &Matrix, j: usize) {
@@ -33,6 +31,54 @@ impl Matrix {
             unsafe{ self.s_product_sum(a, b, j); }
         }
     }
+
+    #[target_feature(enable = "avx2")]
+    #[target_feature(enable = "fma")]
+    unsafe fn row_oplu(&mut self, j: usize){
+        // ref: http://www.personal.psu.edu/jhm/f90/lectures/lu.html
+        let pivot = self.data[j*self.cols+j];
+        for i in j+1..self.rows {
+            // cast ref to ptr then back to ref so we can get mutable and immutable views at once
+            let in_view = & *(self.data.get_unchecked(j*self.cols..(j+1)*self.cols) as *const [f32]);
+            let out_view = &mut *(self.data.get_unchecked_mut(i*self.cols..(i+1)*self.cols) as *mut [f32]);
+            let s = -(out_view[j] / pivot);
+            for k in j+i..self.cols {
+                out_view[k] = s.mul_add(in_view[k], out_view[k]);
+            }
+            out_view[j] = -s;
+        }
+    }
+
+    #[target_feature(enable = "fma")]
+    unsafe fn dot_unsafe(a: &[f32], b: &[f32], len: usize) -> f32 {
+        let mut s = 0.0f32;
+        for i in 0..len {
+            s = a[i].mul_add(b[i], s);
+        }
+        return s;
+    }
+
+    fn solve(&self, b: &Vec<f32>, x: &mut Vec<f32>){
+        // create an augmented matrix in row major order
+        let mut aug = Matrix { rows: self.rows, cols: self.cols+1, data: Vec::with_capacity(self.rows * (self.cols+1)) };
+        for i in 0..aug.rows {
+            let mut p = i;
+            for _j in 0..self.cols {
+                aug.data.push(self.data[p]);
+                p += self.rows;
+            }
+            aug.data.push(b[i]);
+        }
+        // LU decomposition
+        for j in 0..aug.rows {
+            unsafe{ aug.row_oplu(j); }
+        }
+        println!("{:?}", aug.data);
+        // back substitution
+        for j in (0..aug.rows).rev() {
+            unsafe{ x[j] = (b[j] - Matrix::dot_unsafe(&aug.data[j*aug.cols+j+1..], &x[j+1..], aug.rows-1-j)) / aug.data[j*aug.cols+j]; }
+        }
+    }
 }
 
 fn main() {
@@ -41,4 +87,10 @@ fn main() {
     let mut c = Matrix { rows: 2, cols: 2, data: vec![0.0; 4] };
     c.matmul(&a, &b);
     println!("{:?}", c.data);
+
+    let a = Matrix { rows: 3, cols: 3, data: vec![1.0, 4.0, 2.0, 2.0, 3.0, 2.0, -1.0, 1.0, 3.0] };
+    let b = vec![2.0, 3.0, 5.0];
+    let mut x = vec![0.0; 3];
+    a.solve(&b, &mut x);
+    println!("{:?}", x);
 }
