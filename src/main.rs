1762a0cb84dafe75b63313f0a6288f2c1952593e
Alex Yang <aleozlx@gmail.com>
2018-07-04 17:55:31 -0500

    + linear regression

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,80 @@ struct Matrix {
     data: Vec<f32>
 }
 
+trait IColumnVector {
+    fn len(&self) -> usize;
+    fn get_view(&self, j: usize) -> &[f32];
+    fn get_view_mut(&mut self, j: usize) -> &mut [f32];
+}
+
+impl IColumnVector for Matrix {
+    fn len(&self) -> usize {
+        self.rows
+    }
+
+    fn get_view(&self, j: usize) -> &[f32] {
+        &self.data[j*self.rows..(j+1)*self.rows]
+    }
+
+    fn get_view_mut(&mut self, j: usize) -> &mut [f32] {
+        &mut self.data[j*self.rows..(j+1)*self.rows]
+    }
+}
+
+impl IColumnVector for Vec<f32> {
+    fn len(&self) -> usize {
+        self.len()
+    }
+
+    fn get_view(&self, _j: usize) -> &[f32] {
+        &self[..]
+    }
+
+    fn get_view_mut(&mut self, _j: usize) -> &mut [f32] {
+        &mut self[..]
+    }
+}
+
+impl<'a> IColumnVector for &'a [f32] {
+    fn len(&self) -> usize {
+        self[..].len()
+    }
+
+    fn get_view(&self, _j: usize) -> &[f32] {
+        self
+    }
+
+    fn get_view_mut(&mut self, _j: usize) -> &mut [f32] {
+        unimplemented!();
+    }
+}
+
+impl<'a, 'b> std::ops::Mul<&'b Matrix> for &'a Matrix {
+    type Output = Matrix;
+
+    fn mul(self, rhs: &'b Matrix) -> Matrix {
+        let mut ret = Matrix { rows: self.rows, cols: rhs.cols, data: vec![0.0; self.rows*rhs.cols] };
+        ret.matmul(self, rhs);
+        return ret;
+    }
+}
+
 impl Matrix {
     #[target_feature(enable = "avx2")]
     #[target_feature(enable = "fma")]
-    unsafe fn s_product_sum(&mut self, a: &Matrix, b: &Matrix, j: usize) {
-        let s_view = &b.data[j*b.rows..(j+1)*b.rows];
-        // grab the resulting column
-        let out_view = &mut self.data[j*self.rows..(j+1)*self.rows];
+    unsafe fn s_product_sum(out: &mut IColumnVector, a: &Matrix, b: &IColumnVector, j: usize) {
+        let n = out.len();
+        let s_view = b.get_view(j);
+        let out_view = out.get_view_mut(j);
         // reset output space to prepare for accumulation
-        for p in 0..self.rows {
+        for p in 0..n {
             out_view[p] = 0.0;
         }
 
         for k in 0..a.cols {
             let scalar = s_view[k];
             let in_view = &a.data[k*a.rows..(k+1)*a.rows];
-            for p in 0..self.rows {
+            for p in 0..n {
                 // the following can be accelerated using FMA
                 out_view[p] = scalar.mul_add(in_view[p], out_view[p]);
             }
@@ -28,7 +86,9 @@ impl Matrix {
 
     fn matmul(&mut self, a: &Matrix, b: &Matrix) {
         for j in 0..b.cols {
-            unsafe{ self.s_product_sum(a, b, j); }
+            unsafe{
+                Matrix::s_product_sum(self, a, b, j);
+            }
         }
     }
 
@@ -58,6 +118,10 @@ impl Matrix {
         return s;
     }
 
+    fn dot_vecs(a: &IColumnVector, b: &IColumnVector) -> f32 {
+        unsafe { Matrix::dot_unsafe(a.get_view(0), b.get_view(0), std::cmp::min(a.len(), b.len())) }
+    }
+
     fn solve(&self, b: &Vec<f32>, x: &mut Vec<f32>){
         // create an augmented matrix in row major order
         let mut aug = Matrix { rows: self.rows, cols: self.cols+1, data: Vec::with_capacity(self.rows * (self.cols+1)) };
@@ -75,16 +139,35 @@ impl Matrix {
         }
         // back substitution
         for j in (0..aug.rows).rev() {
-            unsafe{ x[j] = (aug.data[(j+1)*aug.cols-1] - Matrix::dot_unsafe(&aug.data[j*aug.cols+j+1..], &x[j+1..], aug.rows-1-j)) / aug.data[j*aug.cols+j]; }
+            x[j] = (aug.data[(j+1)*aug.cols-1] - Matrix::dot_vecs(&&aug.data[j*aug.cols+j+1..], &&x[j+1..])) / aug.data[j*aug.cols+j];
         }
+    }
+
+    fn transpose(&self) -> Matrix {
+        let mut transpose = Matrix { rows: self.cols, cols: self.rows, data: Vec::with_capacity(self.data.len()) };
+        for j in 0..transpose.cols {
+            let mut p = j;
+            for _i in 0..transpose.rows {
+                transpose.data.push(self.data[p]);
+                p += self.cols;
+            }
+        }
+        return transpose;
+    }
+
+    fn linear_regression(&self, y: &Vec<f32>, theta: &mut Vec<f32>){
+        let transpose = self.transpose();
+        let a = &transpose * self;
+        let mut b = vec![0.0; self.cols];
+        unsafe { Matrix::s_product_sum(&mut b, &transpose, y, 0); }
+        a.solve(&b, theta);
     }
 }
 
 fn main() {
     let a = Matrix { rows: 2, cols: 2, data: vec![2.0, 0.1, 0.3, 3.0] };
     let b = Matrix { rows: 2, cols: 2, data: vec![1.0, 0.5, 0.5, 1.0] };
-    let mut c = Matrix { rows: 2, cols: 2, data: vec![0.0; 4] };
-    c.matmul(&a, &b);
+    let c = &a * &b;
     println!("{:?}", c.data);
 
     let a = Matrix { rows: 3, cols: 3, data: vec![1.0, 4.0, 2.0, 2.0, 3.0, 2.0, -1.0, 1.0, 3.0] };
